839138a4d69a74ca4ff35767f044f51eb56014ee
JEROLPOA2 <jledesmap@unal.edu.co>
2022-09-19 22:52:45 -0500

    16

--- src/test/java/gestion/Zona.java
+++ src/test/java/gestion/Zona.java
@@ -7,7 +7,7 @@ public class Zona {
 
     private String nombre;
     private Zoologico zoo;
-    private ArrayList<Animal> animales;
+    private ArrayList<Animal> animales = new ArrayList<Animal>();
 
     public Zona(String nombre, Zoologico zoo){
         
